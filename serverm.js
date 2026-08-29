e22fd79819a77e7c7f0dd18c5c82f2d3f81345ac
mine-roth <mineroth8@gmail.com>
2026-08-29 09:30:49 -0700

    Update fetch URL and API keys in runTask function

--- serverm.js
+++ serverm.js
@@ -2,10 +2,10 @@
 
 async function runTask() {
     try {
-      const response = await fetch("https://wagiunkbuiylyjplirba.supabase.co/rest/v1/students?select=id&limit=1", {
+      const response = await fetch("https://hqqykvvumlgoudkikmlx.supabase.co/rest/v1/update?select=id&limit=1", {
         headers: {
-          "apikey": "sb_publishable_fSW0HQ2Swg8p4XzTHAKlRA_VjShom-J",
-          "Authorization": "Bearer sb_publishable_fSW0HQ2Swg8p4XzTHAKlRA_VjShom-J"
+          "apikey": "sb_publishable_WUK6FDzfwvMBMd50a4HHcw_VvV7uMDs",
+          "Authorization": "Bearer sb_publishable_WUK6FDzfwvMBMd50a4HHcw_VvV7uMDs"
         }
       });
   
